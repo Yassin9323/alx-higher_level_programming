a5259782cc3fb786f81e44be83ff31d6487d4e4c
Yassin9323 <yassin.waleed94@gmail.com>
2024-02-12 08:16:26 +0200

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(x + 1, 10):
-        print("{:02d}".format(x * 10 + y), end="\n" if x == 8 and y == 9 else ", ")
+        z = x * 10 + y
+        print("{:02d}".format(z), end="")
+        print(end=", " if z < 89 else "\n")
+    # another solution print("{:02d}".format(x * 10 + y), end="\n" if x == 8 and y == 9 else ", ")
