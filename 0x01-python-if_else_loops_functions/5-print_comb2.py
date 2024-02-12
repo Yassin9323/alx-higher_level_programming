9e4438c738e5a6739f6782df43f283d191e31b8d
Yassin9323 <yassin.waleed94@gmail.com>
2024-02-12 07:48:35 +0200

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 100):
+for x in range(100):
     if x < 10:
         print("0" + "{}".format(str(x)), end=", ")
     else:
-        print("{}".format(str(x)) + "\n" if x == 99 else x, end=", ")
+        ## print("{}".format(str(x)) + "\n" if x == 99 else x, end=", ")
+        print(x, end=", " if x < 99 else "\n")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+for x in range(100):
+    if x < 10:
+        print("0" + "{}".format(str(x)), end=", ")
+    else:
+        print("{}".format(str(x)), end=", " if x < 99 else "\n")
