281de02a3feb95c4eacdffee46aa22b0d18a4606
Yassin9323 <yassin.waleed94@gmail.com>
2024-02-18 17:03:29 +0200

    task 1 advanced edit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,15 @@ if __name__ == "__main__":
     import calculator_1 as x
     from sys import argv
 
-    a = int(argv[1])
-    b = int(argv[3])
-    s = argv[2]
     num_argv = len(argv) - 1
 
     if num_argv != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    s = argv[2]
 
     if s == "+":
         result = x.add(a, b)
