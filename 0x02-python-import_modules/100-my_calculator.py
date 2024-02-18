7d3fc951a2c1e79bbc233426040f43ac8e5c6c6b
Yassin9323 <yassin.waleed94@gmail.com>
2024-02-18 16:56:16 +0200

    Task 1 advanced

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as x
+    from sys import argv
+
+    a = int(argv[1])
+    b = int(argv[3])
+    s = argv[2]
+
+    if s == "+":
+        result = x.add(a, b)
+    elif s == "-":
+        result = x.sub(a, b)
+    elif s == "*":
+        result = x.mul(a, b)
+    elif s == "/":
+        result = x.div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    num_argv = len(argv) - 1
+    if num_argv != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        print(a, s, b, "=", result)
