fff46f58c5e018d5fe468aea1978c8fab9cb1e90
Yassin9323 <yassin.waleed94@gmail.com>
2024-02-18 15:16:45 +0200

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    num_args = len(argv) - 1
+    if num_args == 1:
+        print(num_args, "argument :")
+    else:
+        print(num_args, "arguments :")
+    for x, arg in enumerate(argv[1:], start=1):
+        print(f"{x}: {arg}")
