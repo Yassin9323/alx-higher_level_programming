a5c0eff0ea5619d0d2805ae1700c0bbb767e2824
Yassin9323 <yassin.waleed94@gmail.com>
2024-03-24 07:19:35 +0200

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,10 @@
--- script that creates a table second_table
-CREATE table IF NOT EXISTS second_table (
-
-	`id` INT,
-	`name` VARCHAR(256),
-	`score` INT
-);
-INSERT INTO `second_table`(`id`, `name`, `score`) 
+-- creates table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES
-	(1, 'John', 10 )
-	(2, 'Alex', 3)
-	(3, 'Bob', 14)
-	(4, 'George', 8);
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
